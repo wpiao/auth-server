701e2c74b294bc8bae15840af5dd05049c9c6365
Wenhao Piao <wenhaopiao520@gmail.com>
2020-05-03 12:50:08 -0700

    complete signup route

--- index.js
+++ index.js
@@ -1,32 +1,3 @@
-// 'use strict';
-
-// // third party dependencies
-// const mongoose = require('mongoose');
-// const dotenv = require('dotenv');
-
-// // internal modules
-// const { start } = require('./lib/server.js');
-
-// // access to process.env
-// dotenv.config();
-
-// // connect to mongoDB
-// const MONGODB_URL = process.env.MONGODB_URL;
-// const options = {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// };
-
-// mongoose.connect(MONGODB_URL, options);
-
-// // check if successfully connected to mongodb
-// const db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'connection error: '));
-// db.once('open', () => console.log('Successfully connected to mongoDB!'));
-
-// // start the server
-// start();
-
 'use strict';
 
 const mongoose = require('mongoose');
